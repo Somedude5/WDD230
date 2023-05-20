b4b0b13776d2e84a6cebae72f17e7283729b90cd
Somedude5 <parkerhatch2098@gmail.com>
2023-05-19 20:04:07 -0600

    newest changes

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -3,10 +3,18 @@ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric
 const currentDate = today.toLocaleDateString('en-US', options);
 
 
-
 const currentDateElement = document.querySelector("#currentdate");
 currentDateElement.textContent = currentDate;
 
 
 const lastupdated = document.querySelector("#lastupdated");
 lastupdated.textContent = document.lastModified;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listener to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
