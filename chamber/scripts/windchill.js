6152976778e344275f40cf3d0b2cd1ff8a2fdad3
Somedude5 <parkerhatch2098@gmail.com>
2023-05-28 16:52:14 -0600

    newest changes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,22 +1,20 @@
-function windchillApp(windspeed, temperature){
-    //Get html elements to update
-    const temperatureElement = document.querySelector("#temperature")
-    const windspeedElement = document.querySelector("#windspeed")
-    const windchillElement = document.querySelector("#windchill")
-    //Calculate the windchill, if applicable
-    let windchillMSG = "N/A"
+function calculateWindchill(windspeed = 0, temperature = 0) {
+  const temperatureElement = document.querySelector("#temperature");
+  const windspeedElement = document.querySelector("#windspeed");
+  const windchillElement = document.querySelector("#windchill");
 
-    if(windspeed >= 3.0 && temperature <= 50){
-        chillfactor = Math.pow(windspeed, 0.16)
-        let windchillamt = 35.74 + (0.6215 * temperature) - (35.75 * chillfactor) + (0.4275 * temperature * chillfactor)
-        windchillMSG = `${windchillamt}`
-    }
-    //update the html elements with values
-    temperatureElement.textContent = temperature
-    windchillElement.textContent = windchillMSG
-    windspeedElement.textContent = windspeed
+  let windchillValue = "N/A";
 
+  if (windspeed >= 3.0 && temperature <= 50) {
+    const chillfactor = Math.pow(windspeed, 0.16);
+    const windchillamt = 35.74 + 0.6215 * temperature - 35.75 * chillfactor + 0.4275 * temperature * chillfactor;
+    windchillValue = `${Math.ceil(windchillamt)}`;
+  }
+
+  temperatureElement.textContent = temperature;
+  windchillElement.textContent = windchillValue;
+  windspeedElement.textContent = windspeed;
 }
 
-//call the windchill function with 
-windchill(5,5)
+// Call the windchill function
+calculateWindchill(5, 5);
